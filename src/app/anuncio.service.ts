ce62694f3800ecf65c7b5c459c649fe9f4f0ee16
LeGorge <andersongmoura@gmail.com>
2019-10-03 23:32:17 -0300

    issue #15 completed

--- src/app/anuncio.service.ts
+++ src/app/anuncio.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Anuncio } from './anuncio';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AnuncioService {
 
-  constructor() { }
+  constructor(private _db:AngularFirestore) { }
+
+  save(anuncio:Anuncio){
+    this._db.collection('anuncios').add(anuncio)
+  }
+
+  /* USAGE:
+  findAll().subscribe(anuncios=>{
+    //do something with anuncios
+  })
+  */
+  findAll():Observable<Anuncio[]>{
+    return this._db.collection('anuncios').valueChanges({idField:'id'}) as Observable<Anuncio[]>
+  }
 }
