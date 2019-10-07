07b3b5840c7039f74fceb6b5d357ec24b1a9abd4
Luiz Guilherme <guilhermemera@hotmail.com>
2019-10-07 14:48:07 -0300

    pagina HTML issue#16

---
+++ src/app/cadastro-anuncio/cadastro-anuncio.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-anuncio',
+  templateUrl: './cadastro-anuncio.page.html',
+  styleUrls: ['./cadastro-anuncio.page.scss'],
+})
+export class CadastroAnuncioPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
