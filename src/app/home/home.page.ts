02971af84f367b1ade48720f5b2161e1c12d22d9
Rodrigo <spensmusic@gmail.com>
2019-10-07 14:45:38 -0300

    interface inicial da Issue #17

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,4 +9,17 @@ export class HomePage {
 
   constructor() {}
 
+  voltar() {
+
+  }
+
+  doRefresh(event) {
+    console.log('Begin async operation');
+
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      event.target.complete();
+    }, 2000);
+  }
+
 }
