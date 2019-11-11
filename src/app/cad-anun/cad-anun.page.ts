71b1a368738d46270387868d1cf0425b416f7da0
Rodrigo <spensmusic@gmail.com>
2019-11-11 10:30:51 -0300

    #25 cadastro de anunciante

---
+++ src/app/cad-anun/cad-anun.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cad-anun',
+  templateUrl: './cad-anun.page.html',
+  styleUrls: ['./cad-anun.page.scss'],
+})
+export class CadAnunPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
