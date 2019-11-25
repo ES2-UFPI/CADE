3c8375650d4a0101d64a3ee4e013a98e9745ffe7
Luiz Guilherme <guilhermemera@hotmail.com>
2019-11-25 15:25:22 -0300

    correção para tentativa de pull request

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,6 @@
 export const environment = {
   production: true,
   firebase: {
-<<<<<<< HEAD
-=======
-
->>>>>>> f349dfd6e1930a4f70327a5c913718213984a606
     apiKey: "AIzaSyCOYRJogIfohRMkib8Td1Rpyk3mTFKCecQ",
     authDomain: "cade-eed42.firebaseapp.com",
     databaseURL: "https://cade-eed42.firebaseio.com",
@@ -12,9 +8,5 @@ export const environment = {
     storageBucket: "cade-eed42.appspot.com",
     messagingSenderId: "1054435576619",
     appId: "1:1054435576619:web:bbf6e5ff25535b8a57936e"
-<<<<<<< HEAD
-=======
-
->>>>>>> f349dfd6e1930a4f70327a5c913718213984a606
   }
 };
