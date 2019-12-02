61bf2c799925c5c97749bbbdd01cf2072f94c86b
Rodrigo <spensmusic@gmail.com>
2019-12-02 15:48:42 -0300

    #53 implementação de alguns métodos e atualização na homepage

--- src/app/anuncio.ts
+++ src/app/anuncio.ts
@@ -1,9 +1,9 @@
 export interface Anuncio {
     id?: string;
-    geolocalizacao: {lat:number, lng:number};
-    titulo: string;
-    descricao: string;
-    categoria: string;
-    dataInicial: Date;
-    dataFinal: Date;
+    geolocalizacao?: {lat:number, lng:number};
+    titulo?: string;
+    descricao?: string;
+    categoria?: string;
+    dataInicial?: Date;
+    dataFinal?: Date;
 }
--- src/app/comentario.service.ts
+++ src/app/comentario.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Comentario } from './comentario';
+import { Observable, forkJoin, of, from, combineLatest } from 'rxjs';
+import { Anuncio } from './anuncio';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ComentarioService {
+  comentarioLocal:Comentario
 
-  constructor() { }
+  constructor(private _db: AngularFirestore) { }
+
+  svLocal(co:Comentario){
+    this.comentarioLocal = co
+  }
+  
+  ldLocal():Comentario{
+    return this.comentarioLocal
+  }
+
+  sv(comentario: Comentario) {
+    if(comentario.id){
+      this._db.collection('comentarios').doc(comentario.id).set(comentario);
+    }else{
+      this._db.collection('comentarios').add(comentario);
+    }
+  }
+
+  findByAnuncioID(anuncio: Anuncio, comentario: Comentario):Observable<Comentario[][]>{
+    var observables:Observable<Comentario[]>[] = []
+
+      /* observables.push(this._db.collection('comentarios',ref => ref.where(comentario.id_do_anuncio, '==', anuncio.id))
+      .valueChanges({idField:'id'}) as Observable<Comentario[]>) */
+
+      observables.push(this._db.collection('comentarios',ref => ref.where(comentario.id_do_anuncio, '==', anuncio.id))
+      .valueChanges() as Observable<Comentario[]>)
+
+    return combineLatest(observables)
+  }
 }
